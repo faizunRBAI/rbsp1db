78af7b87264997e00283433818e8ede478e06e42
faizunRBAI <faizun@royalbengal.ai>
2026-07-08 22:24:57 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/test/java/com/example/rbsp1db/Rbsp1dbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rbsp1db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rbsp1dbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
